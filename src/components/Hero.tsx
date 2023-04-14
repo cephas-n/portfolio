dce731b3d4ed301738b95a3c80146ec0a6de0003
cephas-n <cephas.developer@gmail.com>
2023-04-14 19:01:35 +0300

    make hero section responsive

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,29 +6,58 @@ const Hero = () => {
     <Flex
       justifyContent="center"
       alignItems="flex-end"
-      minHeight="600px"
+      minHeight={{
+        base: "400px",
+        sm: "600px",
+      }}
       position="relative"
     >
       <Heading
         as="h1"
         position="absolute"
-        top="35%"
+        top={{
+          base: "0%",
+          md: "2%",
+          lg: "30%",
+        }}
         left="7%"
         width="min-content"
-        lineHeight={0.9}
+        lineHeight={{
+          base: 0.9,
+          sm: 1.2,
+        }}
         fontWeight="semibold"
+        fontSize={{ base: "5xl", sm: "6xl" }}
       >
-        <Text as="span" fontSize="7xl">
+        <Text as="span" fontSize="inherit">
           Cephas
         </Text>
-        <Text as="span" fontSize="7xl" display="block" marginLeft={40}>
+        <Text
+          as="span"
+          fontSize="inherit"
+          display="block"
+          marginLeft={{
+            base: 20,
+            sm: 24,
+            lg: 24,
+          }}
+        >
           Naweji
         </Text>
       </Heading>
       <Box className="hero-image">
         <Image src={myPhoto} width={600} boxShadow="1px" />
       </Box>
-      <Text fontSize={24} position="absolute" bottom="30%" left="68%">
+      <Text
+        fontSize={24}
+        position="absolute"
+        bottom={{
+          base: 0,
+          md: "15%",
+          lg: "30%",
+        }}
+        left="68%"
+      >
         I'm a{" "}
         <Text as="span" color="primary" fontSize={24}>
           Web developer
