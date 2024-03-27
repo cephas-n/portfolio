e89058933c415d5cee3ecbcde425c51dac2aa433
cephas-n <cephas.developer@gmail.com>
2024-03-27 18:47:56 +0200

    update enerbuildpower website url

--- src/services/projects.ts
+++ src/services/projects.ts
@@ -1,4 +1,3 @@
-
 export interface ProjectType {
   id: string | number;
   name: string;
@@ -19,15 +18,6 @@ const projects: ProjectType[] = [
     type: "Case study",
   },
   {
-    id: 2,
-    name: "EnerBuild Power S.A.R.L",
-    url: "https://enerbuildpower.com",
-    description:
-      "The project involved developing a brand new website for a renewable energy company based in the Democratic Republic of Congo (DRC). The goal was to create a modern and informative website that showcased the company's services and products, while also highlighting their commitment to sustainable energy solutions. The website was designed with a clean and user-friendly interface, and included features such as contact forms, social media integration, and a blog section.",
-    img: "/images/enerbuild-power.webp",
-    type: "Brand New website",
-  },
-  {
     id: 3,
     name: "Orbeet Consulting",
     url: "https://orbeetconsulting.net",
@@ -35,6 +25,15 @@ const projects: ProjectType[] = [
       "The goal was to improve the website's overall user experience by making it more visually appealing, user-friendly, and mobile-responsive. Additionally, the website was optimized for search engines to improve its visibility and increase traffic. The project was completed within a specific timeframe and involved collaborating with the client to ensure that their needs and requirements were met.",
     img: "/images/orbeet-consulting.webp",
     type: "WEBSITE DEVELOPMENT & OPTIMIZATION",
+  },
+  {
+    id: 2,
+    name: "EnerBuild Power S.A.R.L",
+    url: "https://enerbuildpower.cefftech.net",
+    description:
+      "The project involved developing a brand new website for a renewable energy company based in the Democratic Republic of Congo (DRC). The goal was to create a modern and informative website that showcased the company's services and products, while also highlighting their commitment to sustainable energy solutions. The website was designed with a clean and user-friendly interface, and included features such as contact forms, social media integration, and a blog section.",
+    img: "/images/enerbuild-power.webp",
+    type: "Brand New website",
   },
 ];
 
